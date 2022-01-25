6af1a7e354e29f9a4e0e1dd86d9f203550cfa013
tlaroy <tlaroy@comcast.net>
2022-01-25 03:04:33 -0500

    Add files via upload

--- module/const.js
+++ module/const.js
@@ -1,9 +1,9 @@
-export const DEBUG = false;
-export const CONSOLE_GREY    = "#F0F0F0";
-export const CONSOLE_RED     = "#C46C4B";  // D64100
-export const CONSOLE_GREEN   = "#4BC470";
-export const CONSOLE_BLUE    = "#4B73C4";
-export const CONSOLE_DEFAULT = CONSOLE_GREY;
+export const DEBUG               = false;
+export const CONSOLE_GREY        = "#F0F0F0";
+export const CONSOLE_RED         = "#C46C4B";  // D64100
+export const CONSOLE_GREEN       = "#4BC470";
+export const CONSOLE_BLUE        = "#4B73C4";
+export const CONSOLE_DEFAULT     = CONSOLE_GREY;
 export const MIN_FOUNDRY_VERSION = "9.242";
 export const MIN_DND5E_VERSION   = "1.5.7";
 export const MODULE_NAME      	 = "analytics";
